d17ad2a75dc9e3c2b2d5846bf895d4ee74400164
TheRealAbhishekGautam <abhigautamls29@gmail.com>
2023-05-21 15:33:58 +0530

    Next Commit

--- MyProject0/Controllers/CatagoryController.cs
+++ MyProject0/Controllers/CatagoryController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MyProject0.Data;
+using MyProject0.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +12,17 @@ namespace MyProject0.Controllers
 {
     public class CatagoryController : Controller
     {
-        // GET: /<controller>/
+        // Here we are assining all the values from our ApplicationDbContext to a local variable.
+        private readonly ApplicationDbContext _db;
+        public CatagoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        
         public IActionResult Index()
         {
-            return View();
+            List <Catagory> objCatagoryList = _db.Catagories.ToList();
+            return View(objCatagoryList);
         }
     }
 }
