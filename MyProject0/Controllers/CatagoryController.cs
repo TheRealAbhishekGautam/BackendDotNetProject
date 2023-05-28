3cf482b503c95b34224b5725219df85f0b194945
TheRealAbhishekGautam <abhigautamls29@gmail.com>
2023-05-28 22:52:31 +0530

    Next Lessons

--- MyProject0/Controllers/CatagoryController.cs
+++ MyProject0/Controllers/CatagoryController.cs
@@ -24,6 +24,41 @@ namespace MyProject0.Controllers
             List <Catagory> objCatagoryList = _db.Catagories.ToList();
             return View(objCatagoryList);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // We can have action methods of same name but their type should be different.
+        [HttpPost]
+        public IActionResult Create (Catagory obj)
+        {
+            // First we are checking that weather the values that has been returned by the view/user are passing
+            // our Server side validations or not.
+
+            if (ModelState.IsValid)
+            {
+                // Now since we get the values of the object that needs to be added inside the database,
+                // we will use ef core to add those values to the db.
+                _db.Add(obj);
+                // We can add multiple objects then save it once.
+                _db.SaveChanges();
+                // Now after adding the values inside the db, we have to refresh code side too for seeing the updated values.
+                // For that we are using a function RedirectToAction,
+                // Using this we can redirect to any of the action within any of the controller.
+                // RedirectToAction("ActionMethodName","ControllerName)
+                // But if we are calling an action method of the same controller, then we don't need to add the controller name.
+                return RedirectToAction("Index");
+            }
+
+            // Now if we are not able to add the values insde the database then we should be on the same page only and not
+            // Redirected to the page which shows all the updated values.
+            // We also want to display the error the same page only.
+
+            return View();
+            
+        }
     }
 }
 
--- MyProject0/Models/Catagory.cs
+++ MyProject0/Models/Catagory.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace MyProject0.Models
@@ -12,10 +13,19 @@ namespace MyProject0.Models
 	// Always try to install all of the nuget packages of the same version (7.0.5 in this case).
 	public class Catagory
 	{
-		[Key] // Primary Key, will not have the duplicate values
+		[Key]									// Primary Key, will not have the duplicate values
 		public int ID { get; set; }
-		[Required] // NOT NULL inside the sql tables
+
+		[Required]								// NOT NULL inside the sql tables
+		[DisplayName("Catagory Name")]			// It is an annotation for the servr side.
+		[MaxLength(30)]							// These are server side validations.
 		public String Name { get; set; }
+
+		[DisplayName("Display Order")]
+		[Range(1,100,ErrorMessage ="The range allowed is between 1-100")]	// This is also a server side validation.
+																			// ErrorMessage is used to print our custom error message
+																			// If our validation fails.
+																			// If we don't use it then a by-default message will be printed.
 		public int DisplayOrder { get; set; }
 	}
 }
