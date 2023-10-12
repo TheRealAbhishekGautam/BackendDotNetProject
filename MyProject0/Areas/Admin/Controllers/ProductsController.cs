974cf1ab80d97d103e5f913e438566752f60d9ac
Abhishek Gautam <abhigautamls29@gmail.com>
2023-10-13 00:52:35 +0530

    Combining the Update and Insert funcitonality into a single API and View.

--- MyProject0/Areas/Admin/Controllers/ProductsController.cs
+++ MyProject0/Areas/Admin/Controllers/ProductsController.cs
@@ -21,6 +21,60 @@ namespace MyProject0.Areas.Admin.Controllers
             List<Product> productsList = _UnitOfWork.Product.GetAll().ToList();
             return View(productsList);
         }
+        // Upsert is a combination of Update and Insert i.e. we are doing both of the things inside the same view.
+        // So we will have two API's one for returning the respective fields in which we can create a new product or update an existing product
+        // And another one in which we are making an post call.
+        // If the id that has been passes here is null then it's insert otherwise it will be update.
+        // For both of them we will have the same view. (Advantage of doing this)
+        public IActionResult Upsert (int? id)
+        {
+            IEnumerable<SelectListItem> MyCatagoryList = _UnitOfWork.Catagory.GetAll()
+                                                       .Select(x => new SelectListItem()
+                                                       {
+                                                           Text = x.Name,
+                                                           Value = x.ID.ToString()
+                                                       });
+            ProductsVM obj = new ProductsVM()
+            {
+                CatagoryList = MyCatagoryList,
+                Product = new Product()
+            };
+            // Insert case
+            if(id == null || id == 0)
+            {
+                // Just return the regular view of insert.
+                return View(obj);
+            }
+            else
+            {
+                obj.Product = _UnitOfWork.Product.Get(x => x.Id == id); 
+                return View(obj);
+            }
+
+        }
+        [HttpPost]
+        public IActionResult Upsert (ProductsVM obj, IFormFile? file)
+        {
+            if (ModelState.IsValid)
+            {
+                _UnitOfWork.Product.Add(obj.Product);
+                _UnitOfWork.Save();
+                TempData["Success"] = "The Product has been added successfully";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                // We are sending this list again because old one will be gone.
+                obj.CatagoryList = _UnitOfWork.Catagory.GetAll().Select(x => new SelectListItem()
+                {
+                    Text = x.Name,
+                    Value = x.ID.ToString()
+                });
+                return View(obj);
+            }
+        }
+        // We have combined both of the funcitonality in the same Upsert API's so we don't need these anymore.
+        /*
         public IActionResult Create()
         {
             // This is called Projections in .Net, It's a very powerful thing since we are actually converting
@@ -94,6 +148,7 @@ namespace MyProject0.Areas.Admin.Controllers
             return View();
 
         }
+        */
         public IActionResult Delete(int? id)
         {
             if (id == 0 || id == null)
