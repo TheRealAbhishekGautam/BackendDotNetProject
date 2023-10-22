62cdf901a2aaa832693154ea780ba06e7eaca60d
Abhishek Gautam <abhigautamls29@gmail.com>
2023-10-22 10:13:15 +0530

    Custom Update Logic

--- MyProject0.DataAccess/Repository/ProductRepository.cs
+++ MyProject0.DataAccess/Repository/ProductRepository.cs
@@ -17,7 +17,28 @@ namespace MyProject0.DataAccess.Repository
             _db = db;
         }
         public void Update (Product obj) { 
+            // This is the update by ef core
             _db.Product.Update(obj);
+
+            // We can also do our custom updation
+            // In which we can add our logic to update too.
+            var ProductFromDb = _db.Product.FirstOrDefault(x=> x.Id == obj.Id);
+            if (ProductFromDb != null)
+            {
+                ProductFromDb.Title = obj.Title;
+                ProductFromDb.ISBN = obj.ISBN;
+                ProductFromDb.Price = obj.Price;
+                ProductFromDb.Price50 = obj.Price50;
+                ProductFromDb.Price100 = obj.Price100;
+                ProductFromDb.ListPrice = obj.ListPrice;
+                ProductFromDb.Description = obj.Description;
+                ProductFromDb.CatagoryId = obj.CatagoryId;
+                ProductFromDb.Author = obj.Author;
+
+                if(ProductFromDb.ImageUrl != null) {
+                    ProductFromDb.ImageUrl = obj.ImageUrl;
+                }
+            }
         }
     }
 }
